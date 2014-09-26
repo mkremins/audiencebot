c460dd4a60e754857ae541939d7e96b7eaf492e0
Max Kreminski <maxkreminski@gmail.com>
2014-09-26 16:11:45 -0700

    websockets

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var serveStatic = require('serve-static');
+var io = require('socket.io')(server);
 
 var port = 3000;
 
 app.use(serveStatic('public'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 server.listen(port, function(){
   console.log('listening on port ' + port);
