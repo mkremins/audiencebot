1cc77762ec59de3f62db5e0c95e3c2d474fbde6e
Max Kreminski <maxkreminski@gmail.com>
2014-09-26 15:56:50 -0700

    serve static files

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var app = require('express')();
 var server = require('http').Server(app);
+var serveStatic = require('serve-static');
 
 var port = 3000;
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+app.use(serveStatic('public'));
 
 server.listen(port, function(){
   console.log('listening on port ' + port);
