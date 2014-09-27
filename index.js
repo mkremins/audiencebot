29395081d72a24f64e63d854b8be36a19d51cd0a
Max Kreminski <maxkreminski@gmail.com>
2014-09-27 10:51:51 -0700

    track consensus and broadcast to clients on change

--- index.js
+++ index.js
@@ -7,13 +7,34 @@ var port = 3000;
 
 app.use(serveStatic('public'));
 
+var votes = {};
+var consensus = {x: 50, y: 50};
+
+function calculateConsensus(votes){
+  var totalX = 0, totalY = 0, voteCount = 0;
+  for (var key in votes) {
+    totalX += votes[key].x;
+    totalY += votes[key].y;
+    voteCount++;
+  }
+  return {x: totalX / voteCount, y: totalY / voteCount};
+}
+
 io.on('connection', function(socket){
   console.log(socket.id + ': connected');
-  socket.on('move', function(data){
+
+  socket.on('vote changed', function(data){
     console.log(socket.id + ': ' + data.x + ', ' + data.y);
+    votes[socket.id] = data;
+    consensus = calculateConsensus(votes);
+    io.emit('consensus changed', consensus);
   });
+
   socket.on('disconnect', function(){
     console.log(socket.id + ': disconnected');
+    delete votes[socket.id];
+    consensus = calculateConsensus(votes);
+    io.emit('consensus changed', consensus);
   });
 });
 
