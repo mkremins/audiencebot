e7f0cdb391639dba23b639f8a73fa314a4192666
Max Kreminski <maxkreminski@gmail.com>
2014-09-26 18:44:43 -0700

    send move events to server

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(serveStatic('public'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('move', function(data){
+    console.log(data);
+  });
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+  });
 });
 
 server.listen(port, function(){
