659426be5d4c4b053c1f0d90561d35037cbbac0d
Max Kreminski <maxkreminski@gmail.com>
2014-09-30 09:16:21 -0700

    osc

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var serveStatic = require('serve-static');
 var io = require('socket.io')(server);
+var osc = require('node-osc');
 
 var port = 3000;
 
@@ -9,6 +10,7 @@ app.use(serveStatic('public'));
 
 var votes = {};
 var consensus = {x: 50, y: 50};
+var oscClient = new osc.Client('127.0.0.1', 3333);
 
 function calculateConsensus(votes){
   var totalX = 0, totalY = 0, voteCount = 0;
@@ -18,6 +20,12 @@ function calculateConsensus(votes){
     voteCount++;
   }
   return {x: totalX / voteCount, y: totalY / voteCount};
+}
+
+function updateConsensus(votes){
+  consensus = calculateConsensus(votes);
+  io.emit('consensus changed', consensus);
+  oscClient.send(new osc.Message('/consensus', consensus.x, consensus.y));
 }
 
 io.on('connection', function(socket){
@@ -33,16 +41,14 @@ io.on('connection', function(socket){
     data.id = socket.id;
     socket.broadcast.emit('vote changed', data);
     votes[socket.id] = data;
-    consensus = calculateConsensus(votes);
-    io.emit('consensus changed', consensus);
+    updateConsensus(votes);
   });
 
   socket.on('disconnect', function(){
     console.log(socket.id + ': disconnected');
     socket.broadcast.emit('client disconnected', {id: socket.id});
     delete votes[socket.id];
-    consensus = calculateConsensus(votes);
-    socket.broadcast.emit('consensus changed', consensus);
+    updateConsensus(votes);
   });
 });
 
